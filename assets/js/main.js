338a2b077708130fc2885fb437a6d24cce5ea3b9
Mohithoteldekho123 <148522843+Mohithoteldekho123@users.noreply.github.com>
2023-12-01 12:15:14 +0530

    9

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,30 +12,52 @@ function scrollFunction() {
     }
 }
 
-// read more button 
-// function myFunction() {
-//     var dots = document.getElementById("dots");
-//     var moreText = document.getElementById("more");
-//     var btnText = document.getElementById("myBtn");
+function readMore(ele,eve){
+  eve.preventDefault();
+  var txt = document.querySelector(".read_more_text");
+  txt.classList.toggle("show-few-lines");
   
-//     if (dots.style.display === "none") {
-//       dots.style.display = "inline";
-//       btnText.innerHTML = "Read more"; 
-//       moreText.style.display = "none";
-//     } else {
-//       dots.style.display = "none";
-//       btnText.innerHTML = "Read less"; 
-//       moreText.style.display = "inline";
-//     }
-//   }
-const readMoreBtn = document.querySelector(".read-more-btn");
-const text = document.querySelector(".text");
-
-readMoreBtn.addEventListener("click", (e) => {
-  text.classList.toggle("show-more");
-  if (readMoreBtn.innerText === "Read More") {
-    readMoreBtn.innerText = "Read Less";
-  } else {
-    readMoreBtn.innerText = "Read More";
+  if(txt.classList.contains("show-few-lines")){
+      ele.innerText = "Read More";
+  }else{
+      ele.innerText = "Read Less";
   }
-});
+}
+
+
+
+// select box js 
+function format(item, state) {
+    if (!item.id) {
+      return item.text;
+    }
+    var countryUrl = "https://hatscripts.github.io/circle-flags/flags/";
+    var stateUrl = "https://oxguy3.github.io/flags/svg/us/";
+    var url = state ? stateUrl : countryUrl;
+    var img = $("<img>", {
+      class: "img-flag",
+      width: 26,
+      src: url + item.element.value.toLowerCase() + ".svg"
+    });
+    var span = $("<span>", {
+      text: " " + item.text
+    });
+    span.prepend(img);
+    return span;
+  }
+  
+  $(document).ready(function() {
+    $("#countries").select2({
+      templateResult: function(item) {
+        return format(item, false);
+      }
+    });
+    $("#us-states").select2({
+      templateResult: function(item) {
+        return format(item, true);
+      }
+    });
+  });
+  
+
+// country code select box 
